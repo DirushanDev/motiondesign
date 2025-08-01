fc27e1276a4e606d573fc75077a64684d8ea4056
Rubakumar Dirushan <dirushan@amplifyd.com.au>
2025-08-01 16:33:02 +0530

    added

---
+++ resources/views/Components/header.blade.php
@@ -0,0 +1,24 @@
+<div class="container mx-auto px-4 py-4 flex justify-between items-center bg-black text-white">
+    
+    {{-- Logo --}}
+    <div class="flex-shrink-0">
+        <img src="{{ asset('assets/29ee0e713073bdbe99ec96bfce0b39361bf5f300.gif') }}" alt="Logo" class="h-8 w-auto">
+    </div>
+
+    {{-- Navigation Menu --}}
+    <div class="hidden md:flex space-x-8 text-sm">
+        <a href="#" class="hover:text-gray-300">About</a>
+        <a href="#" class="font-semibold text-white underline">Service</a>
+        <a href="#" class="hover:text-gray-300">Case Study</a>
+        <a href="#" class="hover:text-gray-300">Testimonial</a>
+        <a href="#" class="hover:text-gray-300">FAQ</a>
+    </div>
+
+    {{-- CTA Button --}}
+    <div>
+        <a href="#" class="bg-white text-black font-medium px-5 py-2 rounded-full shadow hover:bg-gray-100 transition">
+            Book a Free Consultation
+        </a>
+    </div>
+
+</div>
