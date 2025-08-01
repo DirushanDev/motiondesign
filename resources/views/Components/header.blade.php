8b47ab1377aeae36a460e40b226787e5d74acb74
Rubakumar Dirushan <dirushan@amplifyd.com.au>
2025-08-01 16:47:56 +0530

    added

--- resources/views/Components/header.blade.php
+++ resources/views/Components/header.blade.php
@@ -1,14 +1,14 @@
-<div class="container mx-auto px-4 py-4 flex justify-between items-center bg-black text-white">
+<div class="container mx-auto px-4 py-4 flex justify-around items-center bg-black text-white">
     
     {{-- Logo --}}
     <div class="flex-shrink-0">
-        <img src="{{ asset('assets/29ee0e713073bdbe99ec96bfce0b39361bf5f300.gif') }}" alt="Logo" class="h-8 w-auto">
+        <img src="{{ asset('assets/29ee0e713073bdbe99ec96bfce0b39361bf5f300.gif') }}" alt="Logo" class="h-8 w-full">
     </div>
 
     {{-- Navigation Menu --}}
-    <div class="hidden md:flex space-x-8 text-sm">
+    <div class="hidden md:flex space-x-8 text-base font-Inter">
         <a href="#" class="hover:text-gray-300">About</a>
-        <a href="#" class="font-semibold text-white underline">Service</a>
+        <a href="#" class="font-semibold text-white ">Service</a>
         <a href="#" class="hover:text-gray-300">Case Study</a>
         <a href="#" class="hover:text-gray-300">Testimonial</a>
         <a href="#" class="hover:text-gray-300">FAQ</a>
@@ -16,7 +16,7 @@
 
     {{-- CTA Button --}}
     <div>
-        <a href="#" class="bg-white text-black font-medium px-5 py-2 rounded-full shadow hover:bg-gray-100 transition">
+        <a href="#" class="bg-white text-black font-semibold px-5 py-2 rounded-full shadow hover:bg-gray-100 transition">
             Book a Free Consultation
         </a>
     </div>
